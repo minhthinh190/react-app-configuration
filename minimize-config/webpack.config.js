dc19f247e31c72640548502e3ee4a3c321ff0717
minhthinh190 <dadzaikitano@gmail.com>
2023-07-20 22:39:49 +0700

    Minium config for React project

---
+++ minimize-config/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  devServer: {
+    port: "8000",
+    static: ["./public"],
+    open: true
+  }
+}
