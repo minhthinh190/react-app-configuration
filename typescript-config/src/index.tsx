6012cda92caa57e8e8b98581a56af3708560f616
minhthinh190 <dadzaikitano@gmail.com>
2023-07-22 23:19:33 +0700

    Fix wrong file directory

--- typescript-config/src/index.tsx
+++ typescript-config/src/index.tsx
@@ -4,5 +4,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root")!);
 root.render(<App />);
