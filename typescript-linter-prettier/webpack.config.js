fa83bc66459d68f3f57126b592367ed50f8ff579
minhthinh190 <dadzaikitano@gmail.com>
2023-07-24 23:08:04 +0700

    ESLint and Prettier config for React TypeScript project

---
+++ typescript-linter-prettier/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ts(x)?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    port: "8000",
+    static: ["./public"],
+    open: true,
+  },
+};
